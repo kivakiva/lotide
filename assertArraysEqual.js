c5795de713e80ed7211cd574fa593907a8c39b8f
Adrian Kiva <akiva@mta.ca>
2021-11-23 12:28:44 -0500

    add assert arrays equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+
+const eqArrays = (arr1, arr2) => {
+  let longLength = Math.max(arr1.length, arr2.length);
+  let test = true;
+  for (let i = 0; i < longLength; i++) {
+    if (arr1[i] !== arr2[i]) {
+      test = false;
+    }
+  }
+  return test;
+};
+
+
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`◙◙◙Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`▲▲▲Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+//test
+console.log(assertArraysEqual([1,2,3],[1,2,3]));
+console.log(assertArraysEqual([1,2,3],[1,2,4]));
+console.log(assertArraysEqual([1,2,3],[1,2,3,4]));
