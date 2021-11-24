b198ff745cb52969658e2384cb0b8ec0179c53b2
Adrian Kiva <akiva@mta.ca>
2021-11-24 13:11:12 -0500

    add middle

---
+++ middle.js
@@ -0,0 +1,30 @@
+const eqArrays = (arr1, arr2) => {
+  let longLength = Math.max(arr1.length, arr2.length);
+  let test = true;
+  for (let i = 0; i < longLength; i++) {
+    if (arr1[i] !== arr2[i]) {
+      test = false;
+    }
+  }
+  return test;
+};
+
+
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`◙◙◙Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`▲▲▲Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const middle = (arr) => {
+  if (arr.length < 3) {return []}
+  else if (arr.length % 2 !== 0) {return [arr[arr.length/2-0.5]]}
+ else if (arr.length % 2 === 0) {return [arr[arr.length/2-1],  arr[arr.length/2]]}
+}
+
+assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
+assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]);
+assertArraysEqual(middle([1,2]), []);
