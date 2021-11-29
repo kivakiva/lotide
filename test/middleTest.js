460d141bdaa9bf1b2857dccc5478f3aea35b8aab
Adrian Kiva <akiva@mta.ca>
2021-11-29 10:25:04 -0500

    add unit test for middle and tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,16 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
-assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]);
-assertArraysEqual(middle([1,2]), []);
+const assert = require('chai').assert;
+
+
+describe("#middle", () => {
+  it("returns [4, 5] for [1,2,3,4,5,6,7,8]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
+  });
+  it("returns [4] for [1,2,3,4,5,6,7]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6,7]),[4]); 
+  });
+  it('returns [] for [1,2]', function () {
+    assert.deepEqual(middle([1,2]), [])
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,19 @@
 const tail = require('../tail');
 const assertEqual = require ('../assertEqual');
 
+const assert = require('chai').assert;
+
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+});
+
+
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 const result2 = tail(["single"]);
 assertEqual(result.length, 2); // ensure we get back two elements
